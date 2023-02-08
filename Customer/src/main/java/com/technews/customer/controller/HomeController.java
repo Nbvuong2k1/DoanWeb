bb8e18fc66ab14adab669b9e9b90aad26dd01657
Nbvuong2k1 <vuongspectre201@gmail.com>
2023-02-08 15:16:10 +0700

    first commit

---
+++ Customer/src/main/java/com/technews/customer/controller/HomeController.java
@@ -0,0 +1,144 @@
+package com.technews.customer.controller;
+
+import com.technews.library.dto.CategoryDto;
+import com.technews.library.dto.PostDto;
+import com.technews.library.model.Category;
+import com.technews.library.model.Post;
+import com.technews.library.service.CategoryService;
+import com.technews.library.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+//    @RequestMapping(value = {"/index", "/"}, method = RequestMethod.GET)
+//    public String index(Model model){
+//
+//        return "index";
+//    }
+
+    @GetMapping("/index")
+    public String index(Model model){
+        List<Category> categories = categoryService.findAll();
+        List<PostDto> postDtos = postService.findALl();
+        model.addAttribute("categories", categories);
+        model.addAttribute("posts", postDtos);
+        return "mainpage";
+    }
+
+    @GetMapping("/posts")
+    public String posts(Model model){
+        List<CategoryDto> categoryDtoList = categoryService.getCategoryAndPost();
+        List<Post> posts = postService.getAllPosts();
+        model.addAttribute("categories", categoryDtoList);
+        model.addAttribute("posts", posts);
+        return "mainpage";
+    }
+
+    @GetMapping("/find-post/{id}")
+    public String findPostById(@PathVariable("id") Long id, Model model){
+//        List<Category> categories = categoryService.findAll();
+        Post post = postService.getPostById(id);
+        Long categoryId = post.getCategory().getId();
+//        List<Post> posts = postService.getRelatedPosts(categoryId);
+        model.addAttribute("post", post);
+//        model.addAttribute("posts", posts);
+        return "articlepage";
+    }
+
+    @GetMapping("/posts-in-category/{id}")
+    public String getPostsInCategory(@PathVariable("id") Long id, Model model){
+        Category category = categoryService.findById(id);
+        Long categoryId = category.getId();
+        List<CategoryDto> categories = categoryService.getCategoryAndPost();
+        List<Post> posts = postService.getPostsInCategory(categoryId);
+        List<Category> cate = categoryService.findAll();
+        model.addAttribute("cate", cate);
+        model.addAttribute("category", category);
+        model.addAttribute("categories", categories);
+        model.addAttribute("posts", posts);
+        return "posts-in-category";
+    }
+
+//    @GetMapping("/posts-in-category/{id}")
+//    public String getPostsInCategory(@PathVariable("id") Long categoryId, Model model){
+//        Category category = categoryService.findById(categoryId);
+//        List<CategoryDto> categories = categoryService.getCategoryAndPost();
+//        List<Post> posts = postService.getPostsInCategory(categoryId);
+//        model.addAttribute("category", category);
+//        model.addAttribute("categories", categories);
+//        model.addAttribute("posts", posts);
+//        return "posts-in-category";
+//    }
+
+//    @GetMapping("/find-posts/{id}")
+//    public String findPostsById(@PathVariable("id") Long id, Model model){
+//        Post post = postService.getPostById(id);
+//        Long categoryId = post.getCategory().getId();
+//        List<Post> posts = postService.getRelatedPosts(categoryId);
+//        model.addAttribute("post", post);
+//        model.addAttribute("posts", posts);
+//        return "posts-in-category";
+//    }
+
+
+    @GetMapping("/post")
+    public String post(Model model) {
+
+        List<PostDto> postDtoList = postService.findALl();
+        model.addAttribute("title", "Manage Post");
+        model.addAttribute("posts", postDtoList);
+        model.addAttribute("size", postDtoList.size());
+        return "result-post";
+    }
+
+    @GetMapping("/posts/{pageNo}")
+    public String postsPage(@PathVariable("pageNo") int pageNo, Model model){
+        Page<PostDto> posts = postService.pagePosts(pageNo);
+        model.addAttribute("title", "Manage Post");
+        model.addAttribute("size", posts.getSize());
+        model.addAttribute("totalPages", posts.getTotalPages());
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("posts", posts);
+        return "result-post";
+    }
+
+    @GetMapping("/page-index")
+    public String postsPageHome(@RequestParam(value = "page", defaultValue = "1") int pageNo, Model model){
+        Page<Post> posts = postService.findAllPostsByPage(pageNo, 10);
+        model.addAttribute("title", "Manage Post");
+        model.addAttribute("size", posts.getSize());
+        model.addAttribute("totalPagesHome", posts.getTotalPages());
+        model.addAttribute("currentPageHome", pageNo);
+        model.addAttribute("posts", posts);
+        return "mainpage";
+    }
+
+    @GetMapping("/search-results/{pageNo}")
+    public String searchPosts(@PathVariable("pageNo")int pageNo,
+                              @RequestParam("keyword")String keyword,
+                              Model model){
+
+        Page<PostDto> posts = postService.searchPosts(pageNo, keyword);
+        model.addAttribute("title", "Search Result");
+        model.addAttribute("posts", posts);
+        model.addAttribute("size", posts.getSize());
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", posts.getTotalPages());
+        return "result-post";
+    }
+
+}
---
+++ Customer/src/main/resources/static/layout/footer.js
@@ -0,0 +1,93 @@
+let footer =  document.querySelector('.footer');
+footer.innerHTML=`
+            <div class="grid wide">
+                <div class="row">
+                    <a href="#" class="footer-logo">NEWS</a>
+                </div>
+                <div class="row">
+                    <div class="col l-4">
+                        <h5 class="footer-heading">Theo dõi</h5>
+                        <ul class="footer-list">
+                            <li class="footer-item">
+                                <a href="#" class="footer-item__link">
+                                    <i class="fa-brands fa-facebook"></i>
+                                    Facebook
+                                </a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item__link">
+                                    <i class="fa-brands fa-linkedin"></i>
+                                    Linkedin
+                                </a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item__link">
+                                    <i class="fa-brands fa-instagram"></i>
+                                    Instagram
+                                </a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item__link">
+                                    <i class="fa-brands fa-twitter"></i>
+                                    Twitter
+                                </a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item__link">
+                                    <i class="fa-brands fa-youtube"></i>
+                                    Youtube
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col l-4">
+                        <h5 class="footer-heading">Thông tin</h5>
+                        <ul class="footer-list">
+                            <li class="footer-item">
+                                <a href="#" class="footer-item__link">Giấy phép</a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item__link">Thông tin</a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item__link">Hỗ trợ</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col l-4">
+                        <h5 class="footer-heading">Liên hệ</h5>
+                        <ul class="footer-list">
+                            <li class="footer-item">
+                                <a href="#" class="footer-item__link">
+                                    <i class="fa-solid fa-location-dot"></i>
+                                    55 Giải Phóng, quận Hai Bà Trưng, thành phố Hà Nội
+                                </a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item__link">
+                                    <i class="fa-solid fa-phone"></i>
+                                    0359552500
+                                </a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item__link">
+                                    <i class="fa-solid fa-envelope"></i>
+                                    webnews2022@gmail.com
+                                </a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item__link">
+                                    <i class="fa-solid fa-earth-americas"></i>
+                                     www.webnews.com
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col l-4 l-o-5">
+                        <p class="footer-privacy"></p>
+                    </div>
+                </div>
+            </div>
+` 
